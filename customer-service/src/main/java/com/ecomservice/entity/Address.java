d13714b1b4af3818d2dece0aa1628e9dd00e5e6b
Prasadsangam93 <sangamprasad1993@gmail.com>
2025-11-06 18:37:11 +0530

    Remove houseNumber field from Address entity

--- customer-service/src/main/java/com/ecomservice/entity/Address.java
+++ customer-service/src/main/java/com/ecomservice/entity/Address.java
@@ -20,7 +20,7 @@ public class Address {
     private String state;
     private String country;
     private String postalCode;
-    private  String houseNumber;
+    
 
     @ManyToOne
     @JoinColumn(name = "customer_id")
